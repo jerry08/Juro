ca5e1e420c47d61c11e0f9f97b40a18955f152a3
jerrry08 <jeremyyearwood99@gmail.com>
2023-03-15 22:35:33 -0400

    Update

--- Juro.DemoConsole/Program.cs
+++ Juro.DemoConsole/Program.cs
@@ -1,5 +1,8 @@
-﻿using Juro.Clients;
+﻿using System;
+using System.Threading.Tasks;
+using Juro.Clients;
 using Juro.Utils;
+using Juro.DemoConsole.Utils;
 
 namespace Juro.DemoConsole;
 
@@ -9,27 +12,42 @@ internal class Program
     {
         Console.WriteLine("Hello, World!");
 
-        //await TestMovie();
-        await TestManga();
+        await TestMovie();
+        //await TestManga();
     }
 
     static async Task TestMovie()
     {
         var client = new MovieClient();
-        var movies = await client.FlixHQ.SearchAsync("anohana");
+        var movies = await client.FlixHQ.SearchAsync("spongebob");
 
         var movieInfo = await client.FlixHQ.GetMediaInfoAsync(movies[0].Id);
         var servers = await client.FlixHQ.GetEpisodeServersAsync(movieInfo!.Episodes[0].Id, movieInfo!.Id);
+
+        //Defaut
         var sources = await client.FlixHQ.GetEpisodeSourcesAsync(movieInfo!.Episodes[0].Id, movieInfo!.Id);
+        //
+        //var sources = await client.FlixHQ.GetEpisodeSourcesAsync(servers[0].Url, movieInfo!.Id);
+
+        // Download the stream
+        var fileName = $@"{Environment.CurrentDirectory}\test1.ts";
+
+        var downloader = new HlsDownloader();
+
+        using var progress = new ConsoleProgress();
+
+        var qualities = await downloader.GetHlsStreamMetadatasAsync(sources[0].VideoUrl, sources[0].Headers);
+        var stream = await qualities[0].Stream;
+        await downloader.DownloadAllTsThenMergeAsync(stream, sources[0].Headers, fileName, progress, 15);
     }
 
     static async Task TestManga()
     {
         var client = new MangaClient();
         //var results = await client.MangaKakalot.SearchAsync("Tomodachi Game");
-        var results = await client.Mangadex.SearchAsync("solo leveling");
-        var mangaInfo = await client.Mangadex.GetMangaInfoAsync(results[0].Id);
-        var pages = await client.Mangadex.GetChapterPagesAsync(mangaInfo.Chapters[0].Id);
+        var results = await client.MangaKatana.SearchAsync("solo leveling");
+        var mangaInfo = await client.MangaPill.GetMangaInfoAsync(results[0].Id);
+        var pages = await client.MangaPill.GetChapterPagesAsync(mangaInfo.Chapters[0].Id);
 
         // Download the image
         var fileName = $@"{Environment.CurrentDirectory}\page1.png";
