66b91741d6668a5155ff11a77eba22576dce2912
jerrry08 <jeremyyearwood99@gmail.com>
2023-04-11 11:57:31 -0400

    Fix rapidcloud

--- Juro/Extractors/RapidCloud.cs
+++ Juro/Extractors/RapidCloud.cs
@@ -16,9 +16,6 @@ public class RapidCloud : IVideoExtractor
     public string ServerName => "RapidCloud";
 
     private readonly string _fallbackKey = "c1d17096f2ca11b7";
-    //private readonly string _consumetApi = "https://consumet-api.herokuapp.com";
-    private readonly string _consumetApi = "https://api.consumet.org";
-    private readonly string _enimeApi = "https://api.enime.moe";
     private readonly string _host = "https://rapid-cloud.co";
 
     public RapidCloud(Func<HttpClient> httpClientProvider)
@@ -33,52 +30,45 @@ public class RapidCloud : IVideoExtractor
         var http = _httpClientProvider();
 
         var id = new Stack<string>(url.Split('/')).Pop().Split('?')[0];
-        //var sId = await http.ExecuteAsync(consumetApi + "/utils/rapid-cloud", cancellationToken);
-        var sId = await http.ExecuteAsync(
-            $"{_enimeApi}/tool/rapid-cloud/server-id",
-            new Dictionary<string, string>()
-            {
-                { "User-Agent", "Saikou" }
-            },
+
+        var response = await http.ExecuteAsync(
+            $"{_host}/ajax/embed-6/getSources?id={id}",
             cancellationToken
         );
-
-        if (string.IsNullOrWhiteSpace(sId))
-        {
-            sId = await http.ExecuteAsync(
-                $"{_enimeApi}/tool/rapid-cloud/server-id",
-                cancellationToken
-            );
-        }
 
         var headers = new Dictionary<string, string>()
         {
             { "X-Requested-With", "XMLHttpRequest" }
         };
 
-        var res = await http.ExecuteAsync(
-            $"{_host}/ajax/embed-6/getSources?id={id}&sId={sId}",
-            headers,
-            cancellationToken
-        );
-
         var decryptKey = await http.ExecuteAsync(
-            "https://raw.githubusercontent.com/consumet/rapidclown/main/key.txt",
+            "https://raw.githubusercontent.com/enimax-anime/key/e6/key.txt",
             cancellationToken
         );
 
         if (string.IsNullOrWhiteSpace(decryptKey))
             decryptKey = _fallbackKey;
 
-        var jObj = JObject.Parse(res);
+        var jObj = JObject.Parse(response);
 
-        var sources = jObj["sources"]?.ToString()!;
+        var sources = jObj["sources"]?.ToString();
+        if (string.IsNullOrWhiteSpace(sources))
+            return new();
 
         var isEncrypted = (bool)jObj["encrypted"]!;
         if (isEncrypted)
-            sources = new RapidCloudDecryptor().Decrypt(sources, decryptKey);
+        {
+            try
+            {
+                sources = new RapidCloudDecryptor().Decrypt(sources!, decryptKey);
+            }
+            catch
+            {
+                return new();
+            }
+        }
 
-        var m3u8File = JArray.Parse(sources)[0]["file"]?.ToString()!;
+        var m3u8File = JArray.Parse(sources!)[0]["file"]?.ToString()!;
 
         return new List<VideoSource>
         {
