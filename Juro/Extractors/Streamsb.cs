d6e9c9e2416858047dec6bd5468a4dda013e3b28
jerrry08 <jeremyyearwood99@gmail.com>
2023-05-11 12:11:09 -0400

    Fix streamsb

--- Juro/Extractors/Streamsb.cs
+++ Juro/Extractors/Streamsb.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Juro.Models.Videos;
@@ -15,7 +15,7 @@ public class StreamSB : IVideoExtractor
 {
     private readonly Func<HttpClient> _httpClientProvider;
 
-    private readonly char[] hexArray = "0123456789ABCDEF".ToCharArray();
+    private readonly string _alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
     public string ServerName => "StreamSB";
 
@@ -34,15 +34,12 @@ public class StreamSB : IVideoExtractor
         if (string.IsNullOrWhiteSpace(id))
             id = url.Split(new[] { "/e/" }, StringSplitOptions.None)[1];
 
-        var bytes = Encoding.ASCII.GetBytes($"||{id}||||streamsb");
-        var bytesToHex = BytesToHex(bytes);
+        //var source = await http.ExecuteAsync(
+        //    "https://raw.githubusercontent.com/jerry08/anistream-extras/main/streamsb.txt",
+        //    cancellationToken
+        //);
 
-        var source = await http.ExecuteAsync(
-            "https://raw.githubusercontent.com/jerry08/anistream-extras/main/streamsb.txt",
-            cancellationToken
-        );
-
-        var jsonLink = $"{source.Trim()}/{bytesToHex}/";
+        var jsonLink = $"https://sbani.pro/375664356a494546326c4b797c7c6e756577776778623171737/{Encode(id)}";
 
         var headers = new Dictionary<string, string>()
         {
@@ -69,17 +66,30 @@ public class StreamSB : IVideoExtractor
         };
     }
 
-    private string BytesToHex(byte[] bytes)
+    private string Encode(string id)
     {
-        var hexChars = new char[bytes.Length * 2];
-        for (var j = 0; j < bytes.Length; j++)
-        {
-            var v = bytes[j] & 0xFF;
+        id = $"{MakeId(12)}||{id}||{MakeId(12)}||streamsb";
 
-            hexChars[j * 2] = hexArray[v >> 4];
-            hexChars[(j * 2) + 1] = hexArray[v & 0x0F];
+        var output = "";
+        var arr = id.ToArray();
+
+        for (var i = 0; i < id.Length; i++)
+        {
+            output += Convert.ToString(Convert.ToInt32(((int)arr[i]).ToString(), 10), 16);
         }
 
-        return new string(hexChars);
+        return output;
+    }
+
+    private string MakeId(int length)
+    {
+        var output = "";
+
+        for (var i = 0; i < length; i++)
+        {
+            output += _alphabet[(int)Math.Floor(new Random().NextDouble() * _alphabet.Length)];
+        }
+
+        return output;
     }
 }
