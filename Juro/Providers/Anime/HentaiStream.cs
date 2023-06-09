26cd8b0b45ad77697e9c1ce5f057eea348997c5a
jerrry08 <jeremyyearwood99@gmail.com>
2023-06-09 11:13:42 -0400

    Add Providers

---
+++ Juro/Providers/Anime/HentaiStream.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+using Juro.Utils;
+
+namespace Juro.Providers.Anime;
+
+/// <summary>
+/// Client for interacting with Hentai Stream.
+/// </summary>
+public class HentaiStream : HentaiFF
+{
+    public override string Name => "Hentai Stream";
+
+    public override string BaseUrl => "https://hentaistream.moe";
+
+    /// <summary>
+    /// Initializes an instance of <see cref="HentaiStream"/>.
+    /// </summary>
+    public HentaiStream(Func<HttpClient> httpClientProvider) : base(httpClientProvider)
+    {
+    }
+
+    /// <summary>
+    /// Initializes an instance of <see cref="HentaiStream"/>.
+    /// </summary>
+    public HentaiStream() : this(Http.ClientProvider)
+    {
+    }
+}
