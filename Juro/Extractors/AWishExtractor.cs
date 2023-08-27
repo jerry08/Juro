861091347bc76194359f3394c07b5cb76e44ba68
jerrry08 <jeremyyearwood99@gmail.com>
2023-08-27 11:47:19 -0400

    Add extractors

---
+++ Juro/Extractors/AWishExtractor.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using Juro.Models.Videos;
+using Juro.Utils;
+using Juro.Utils.Extensions;
+
+namespace Juro.Extractors;
+
+/// <summary>
+/// Extractor for AWish.
+/// </summary>
+public class AWishExtractor : IVideoExtractor
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    /// <inheritdoc />
+    public string ServerName => "AWish";
+
+    /// <summary>
+    /// Initializes an instance of <see cref="AWishExtractor"/>.
+    /// </summary>
+    public AWishExtractor(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    /// <summary>
+    /// Initializes an instance of <see cref="AWishExtractor"/>.
+    /// </summary>
+    public AWishExtractor(Func<HttpClient> httpClientProvider)
+        : this(new HttpClientFactory(httpClientProvider))
+    {
+    }
+
+    /// <summary>
+    /// Initializes an instance of <see cref="AWishExtractor"/>.
+    /// </summary>
+    public AWishExtractor() : this(Http.ClientProvider)
+    {
+    }
+
+    /// <inheritdoc />
+    public async ValueTask<List<VideoSource>> ExtractAsync(
+        string url,
+        CancellationToken cancellationToken = default)
+    {
+        var http = _httpClientFactory.CreateClient();
+
+        var response = await http.ExecuteAsync(url, cancellationToken);
+
+        var mediaUrl = new Regex("file:\"([^\"]+)\"\\}").Match(response)
+            .Groups.OfType<Group>()
+            .ToList()[1]
+            .Value;
+
+        return new List<VideoSource>
+        {
+            new()
+            {
+                Format = VideoType.M3u8,
+                VideoUrl = mediaUrl,
+                Title = ServerName
+            }
+        };
+    }
+}
---
+++ Juro/Extractors/VidStreamExtractor.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Juro.Models.Videos;
+using Juro.Utils;
+using Juro.Utils.Extensions;
+
+namespace Juro.Extractors;
+
+/// <summary>
+/// Extractor for VidStream.
+/// </summary>
+public class VidStreamExtractor : IVideoExtractor
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    /// <inheritdoc />
+    public string ServerName => "VidStream";
+
+    /// <summary>
+    /// Initializes an instance of <see cref="VidStreamExtractor"/>.
+    /// </summary>
+    public VidStreamExtractor(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    /// <summary>
+    /// Initializes an instance of <see cref="VidStreamExtractor"/>.
+    /// </summary>
+    public VidStreamExtractor(Func<HttpClient> httpClientProvider)
+        : this(new HttpClientFactory(httpClientProvider))
+    {
+    }
+
+    /// <summary>
+    /// Initializes an instance of <see cref="VidStreamExtractor"/>.
+    /// </summary>
+    public VidStreamExtractor() : this(Http.ClientProvider)
+    {
+    }
+
+    /// <inheritdoc />
+    public async ValueTask<List<VideoSource>> ExtractAsync(
+        string url,
+        CancellationToken cancellationToken = default)
+    {
+        var http = _httpClientFactory.CreateClient();
+
+        var response = await http.ExecuteAsync(url, cancellationToken);
+
+        if (url.Contains("srcd"))
+        {
+            var link = response.FindBetween("\"file\": '", "',");
+
+            return new List<VideoSource>
+            {
+                new()
+                {
+                    Format = VideoType.M3u8,
+                    VideoUrl = link,
+                    Title = ServerName
+                }
+            };
+        }
+
+        var document = Html.Parse(response);
+
+        var mediaUrl = document.DocumentNode.SelectSingleNode(".//iframe")?.Attributes["src"]?.Value;
+        if (string.IsNullOrWhiteSpace(mediaUrl))
+            return new();
+
+        if (mediaUrl!.Contains("filemoon"))
+            return await new FilemoonExtractor(_httpClientFactory).ExtractAsync(mediaUrl, cancellationToken);
+
+        return await new GogoCDNExtractor(_httpClientFactory).ExtractAsync(mediaUrl, cancellationToken);
+    }
+}
