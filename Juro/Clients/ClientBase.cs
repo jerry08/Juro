5fcc9c00248b9db74cef94cc858b9702da99ecd4
jerrry08 <jeremyyearwood99@gmail.com>
2023-10-27 15:26:05 -0400

    Cleanup

--- Juro/Clients/ClientBase.cs
+++ Juro/Clients/ClientBase.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
 using Juro.Core;
 using Juro.Core.Providers;
 
--- Juro/Utils/Extensions/ModuleProviderExtensions.cs
+++ Juro/Utils/Extensions/ModuleProviderExtensions.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Juro.Utils.Extensions;
+﻿namespace Juro.Utils.Extensions;
 
 public static class ModuleProviderExtensions
 {
